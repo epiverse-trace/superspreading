c3685ac5c029b29403e375099029e3d15ef275e2
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-05-18 16:58:13 +0100

    updated cases_to_transmission to use numerical and analytical methods, added internal functions for each, WIP #5

--- R/cases_to_transmission.R
+++ R/cases_to_transmission.R
@@ -1,4 +1,5 @@
-#' Estimate what proportion of cases cause a certain proportion of transmission
+#' Estimate what proportion of cases that cause a certain proportion of
+#' transmission
 #'
 #' @description Calculates the proportion of cases that cause a certain
 #' percentage of transmission.
@@ -11,9 +12,15 @@
 #' @details Multiple values of R and k can be supplied and a data frame of
 #' every combination of these will be returned.
 #'
+#' The numerical calculation uses random number generation to simulate secondary
+#' contacts so the answers may minimally vary between calls. The number of
+#' simulation replicates is fixed to `1e5`.
+#'
 #' @inheritParams probability_epidemic
 #' @param percent_transmission A `number` of the percentage transmission
 #' for which a proportion of cases has produced
+#' @param sim A `logical` whether the calculation should be done numerically
+#' (i.e. simulate secondary contacts) or analytically
 #'
 #' @return A data frame with the value for the proportion of cases for a given
 #' value of R and k
@@ -45,39 +52,80 @@
 #'   k = k,
 #'   percent_transmission = percent_transmission
 #' )
-cases_to_transmission <- function(R, k, percent_transmission) {
+cases_to_transmission <- function(R, k, percent_transmission, sim = FALSE) {
 
   # check input
   checkmate::assert_numeric(R)
   checkmate::assert_numeric(k)
   checkmate::assert_number(percent_transmission)
+  checkmate::assert_logical(sim)
 
   df <- expand.grid("R" = R, "k" = k, NA_real_)
   colnames(df) <- c("R", "k", paste0("prop_", percent_transmission * 100))
 
-  nsim <- 1e5
   for (i in seq_len(nrow(df))) {
 
-      simulate_secondary <- stats::rnbinom(
-        n = nsim,
-        mu = df[i, "R"],
-        size = df[i, "k"]
+    if (sim) {
+      prop <- .cases_to_transmission_numerical(
+        R = df[i, "R"],
+        k = df[i, "k"],
+        percent_transmission = percent_transmission
       )
+    } else {
+      prop <- .cases_to_transmission_analytical(
+        R = df[i, "R"],
+        k = df[i, "k"],
+        percent_transmission = percent_transmission
+      )
+    }
 
-      # percentage of cases
-      percent_cases <- percent_transmission * sum(simulate_secondary)
-
-      # cumulative sum of simulated secondary cases
-      cumsum_secondary <- cumsum(sort(simulate_secondary, decreasing = T))
-
-      # proportion causing case
-      out <-  sum(cumsum_secondary <= percent_cases) / nsim
-
-      df$prop_80[i] <- paste0(round(out * 100, digits = 1),"%")
+    # df is ways i x 3 so insert value into col 3
+    df[i, 3] <- paste0(round(prop * 100, digits = 1),"%")
   }
   return(df)
 }
 
+#' Analytical calculation of proportion of cases that cause \eqn{x} percent
+#' of transmission
+#'
+#' @return A numeric
+#' @keywords internal
+#' @noRd
+.cases_to_transmission_analytical <- function(R, k, percent_transmission) {
 
+  xm1 <- stats::qnbinom(1 - percent_transmission, k + 1, mu = R * (k + 1) / k)
+  remq <- 1 - percent_transmission -
+    stats::pnbinom(xm1 - 1, k + 1, mu = R * (k + 1) / k)
+  remx <- remq / stats::dnbinom(xm1, k + 1, mu = R * (k + 1) / k)
+  x <- xm1 + 1
+  out <- 1 - stats::pnbinom(x - 1, k, mu = R) -
+    stats::dnbinom(x, k, mu = R) * remx
+  return(out)
+}
 
+#' Numerical calculation of proportion of cases that cause \eqn{x} percent
+#' of transmission
+#'
+#' @return A numeric
+#' @keywords internal
+#' @noRd
+.cases_to_transmission_numerical <- function(R, k, percent_transmission) {
 
+  nsim <- 1e5
+  simulate_secondary <- stats::rnbinom(
+    n = nsim,
+    mu = R,
+    size = k
+  )
+
+  # percentage of cases
+  percent_cases <- percent_transmission * sum(simulate_secondary)
+
+  # cumulative sum of simulated secondary cases
+  cumsum_secondary <- cumsum(sort(simulate_secondary, decreasing = TRUE))
+
+  # proportion causing case
+  out <-  sum(cumsum_secondary <= percent_cases) / nsim
+
+  return(out)
+}
