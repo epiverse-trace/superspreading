e4daada5f6cfdd09f7ad2551a3448eaf0664b368
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-05-18 16:59:55 +0100

    added more testing for case_to_transmission updates, WIP #5

--- tests/testthat/test-case_to_transmission.R
+++ tests/testthat/test-case_to_transmission.R
@@ -4,7 +4,16 @@ test_that("cases_to_transmission works as expected for single R and k", {
   expect_s3_class(res, "data.frame")
   expect_identical(dim(res), c(1L, 3L))
   expect_identical(
-    unname(sapply(res, class)),
+    unname(vapply(res, class, character(1))),
+    c("numeric", "numeric", "character")
+  )
+
+  res <- cases_to_transmission(R = 2, k = 0.5, percent_transmission = 0.8, sim = TRUE)
+
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(1L, 3L))
+  expect_identical(
+    unname(vapply(res, class, character(1))),
     c("numeric", "numeric", "character")
   )
 })
@@ -19,7 +28,21 @@ test_that("cases_to_transmission works as expected for multiple R", {
   expect_s3_class(res, "data.frame")
   expect_identical(dim(res), c(3L, 3L))
   expect_identical(
-    unname(sapply(res, class)),
+    unname(vapply(res, class, character(1))),
+    c("numeric", "numeric", "character")
+  )
+
+  res <- cases_to_transmission(
+    R = c(1, 2, 3),
+    k = 0.5,
+    percent_transmission = 0.8,
+    sim = TRUE
+  )
+
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(3L, 3L))
+  expect_identical(
+    unname(vapply(res, class, character(1))),
     c("numeric", "numeric", "character")
   )
 })
@@ -34,7 +57,21 @@ test_that("cases_to_transmission works as expected for multiple R & k", {
   expect_s3_class(res, "data.frame")
   expect_identical(dim(res), c(9L, 3L))
   expect_identical(
-    unname(sapply(res, class)),
+    unname(vapply(res, class, character(1))),
+    c("numeric", "numeric", "character")
+  )
+
+  res <- cases_to_transmission(
+    R = c(1, 2, 3),
+    k = c(0.1, 0.2, 0.3),
+    percent_transmission = 0.8,
+    sim = TRUE
+  )
+
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(9L, 3L))
+  expect_identical(
+    unname(vapply(res, class, character(1))),
     c("numeric", "numeric", "character")
   )
 })
@@ -54,5 +91,29 @@ test_that("cases_to_transmission fails as expected", {
     cases_to_transmission(R = 1, k = 0.1, percent_transmission = "0.8"),
     regexp = "Assertion on 'percent_transmission' failed"
   )
+
+  expect_error(
+    cases_to_transmission(R = 1, k = 0.1, percent_transmission = 0.8, sim = 1),
+    regexp = "Assertion on 'sim' failed"
+  )
 })
 
+test_that(".cases_to_transmission_numerical works as expected", {
+  res <- .cases_to_transmission_numerical(
+    R = 2,
+    k = 0.5,
+    percent_transmission = 0.8
+  )
+  expect_type(res, "double")
+  expect_length(res, 1)
+})
+
+test_that(".cases_to_transmission_analytical works as expected", {
+  res <- .cases_to_transmission_analytical(
+    R = 2,
+    k = 0.5,
+    percent_transmission = 0.8
+  )
+  expect_type(res, "double")
+  expect_length(res, 1)
+})
