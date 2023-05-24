3810db834123ea86289ab77cb01418566ed459f8
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-05-24 18:15:19 +0100

    added tests for missing coverage on poisson mixture functions

--- tests/testthat/test-offspring_distributions.R
+++ tests/testthat/test-offspring_distributions.R
@@ -15,6 +15,9 @@ test_that("ppoislnorm works as expected", {
     c(0.3327375980, 0.4796494230, 0.5929054296, 0.6784115447, 0.7429984371,
       0.7922023350, 0.8301160284, 0.8596839995, 0.8830164300, 0.9016343273)
   )
+  expect_true(is.nan(ppoislnorm(q = NaN, meanlog = 1, sdlog = 1)))
+  expect_true(is.na(ppoislnorm(q = NA, meanlog = 1, sdlog = 1)))
+  expect_equal(unname(ppoislnorm(q = "1", meanlog = 1, sdlog = 1)), 0)
 })
 
 test_that("dpisweibull works as expected", {
@@ -34,4 +37,7 @@ test_that("ppoisweibull works as expected", {
     c(0.7499999996, 0.8749999996, 0.9374999998, 0.9687499998, 0.9843749998,
       0.9921874998, 0.9960937498, 0.9980468748, 0.9990234373, 0.9995117185)
   )
+  expect_true(is.nan(ppoisweibull(q = NaN, shape = 1, scale = 1)))
+  expect_true(is.na(ppoisweibull(q = NA, shape = 1, scale = 1)))
+  expect_equal(unname(ppoisweibull(q = "1", shape = 1, scale = 1)), 0)
 })
