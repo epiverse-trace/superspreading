4fe00ae60b26fd3fa34677efeb2312b9d8b0a25a
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-06-02 14:20:16 +0100

    updated test expectations for new lintr

--- tests/testthat/test-probability_epidemic.R
+++ tests/testthat/test-probability_epidemic.R
@@ -1,7 +1,7 @@
 test_that("probability_epidemic works for R < 1", {
-  expect_equal(probability_epidemic(R = 0.5, k = 0.5, a = 1), 0L)
-  expect_equal(probability_epidemic(R = 0.5, k = 0.1, a = 1), 0L)
-  expect_equal(probability_epidemic(R = 0.5, k = 0.5, a = 50), 0L)
+  expect_identical(probability_epidemic(R = 0.5, k = 0.5, a = 1), 0)
+  expect_identical(probability_epidemic(R = 0.5, k = 0.1, a = 1), 0)
+  expect_identical(probability_epidemic(R = 0.5, k = 0.5, a = 50), 0)
 })
 
 test_that("probability_epidemic works for R > 1", {
@@ -17,7 +17,8 @@ test_that("probability_epidemic works for different k & a", {
   res2 <- probability_epidemic(R = 1.5, k = 0.2, a = 1)
   res3 <- probability_epidemic(R = 1.5, k = 0.5, a = 2)
   res4 <- probability_epidemic(R = 1.5, k = 0.2, a = 2)
-  expect_true((res2 < res1) && (res4 < res3))
+  expect_true((res2 < res1))
+  expect_true((res4 < res3))
 })
 
 test_that("probability_epidemic fails correctly", {
@@ -36,9 +37,9 @@ test_that("probability_epidemic fails correctly", {
 })
 
 test_that("probability_extinct works for R < 1", {
-  expect_equal(probability_extinct(R = 0.5, k = 0.5, a = 1), 1L)
-  expect_equal(probability_extinct(R = 0.5, k = 0.1, a = 1), 1L)
-  expect_equal(probability_extinct(R = 0.5, k = 0.5, a = 50), 1L)
+  expect_identical(probability_extinct(R = 0.5, k = 0.5, a = 1), 1)
+  expect_identical(probability_extinct(R = 0.5, k = 0.1, a = 1), 1)
+  expect_identical(probability_extinct(R = 0.5, k = 0.5, a = 50), 1)
 })
 
 test_that("probability_epidemic works for R > 1", {
