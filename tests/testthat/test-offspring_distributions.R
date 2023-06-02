c811b16c4005640335e751fa874cf95ee24691ac
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-06-02 14:20:16 +0100

    updated test expectations for new lintr

--- tests/testthat/test-offspring_distributions.R
+++ tests/testthat/test-offspring_distributions.R
@@ -17,7 +17,7 @@ test_that("ppoislnorm works as expected", {
   )
   expect_true(is.nan(ppoislnorm(q = NaN, meanlog = 1, sdlog = 1)))
   expect_true(is.na(ppoislnorm(q = NA, meanlog = 1, sdlog = 1)))
-  expect_equal(unname(ppoislnorm(q = "1", meanlog = 1, sdlog = 1)), 0)
+  expect_identical(unname(ppoislnorm(q = "1", meanlog = 1, sdlog = 1)), 0)
 })
 
 test_that("dpisweibull works as expected", {
@@ -39,5 +39,5 @@ test_that("ppoisweibull works as expected", {
   )
   expect_true(is.nan(ppoisweibull(q = NaN, shape = 1, scale = 1)))
   expect_true(is.na(ppoisweibull(q = NA, shape = 1, scale = 1)))
-  expect_equal(unname(ppoisweibull(q = "1", shape = 1, scale = 1)), 0)
+  expect_identical(unname(ppoisweibull(q = "1", shape = 1, scale = 1)), 0)
 })
