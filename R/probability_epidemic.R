3a59fdbedfd7536fe3d35e9b098637f695d08bff
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-12-12 18:28:19 +0000

    added individual-level and population-level control to probability_epidemic and probability_extinct, closes #13

--- R/probability_epidemic.R
+++ R/probability_epidemic.R
@@ -10,6 +10,10 @@
 #' @param k A `number` specifying the  k parameter (i.e. overdispersion in
 #' offspring distribution from fitted negative binomial).
 #' @param num_init_infect A `count` specifying the number of initial infections.
+#' @param ind_control A `numeric` specifying the strength of individual-level
+#' control measures. Between `0` (default) and `1` (maximum).
+#' @param pop_control A `numeric` specifying the strength of population-level
+#' control measures. Between `0` (default) and `1` (maximum).
 #' @param ... [dots] not used, extra arguments supplied will cause a warning.
 #' @param offspring_dist An `<epidist>` object. An S3 class for working with
 #' epidemiological parameters/distributions, see [epiparameter::epidist()].
@@ -20,6 +24,10 @@
 #'
 #' @references
 #'
+#' Lloyd-Smith, J. O., Schreiber, S. J., Kopp, P. E., & Getz, W. M. (2005)
+#' Superspreading and the effect of individual variation on disease emergence.
+#' Nature, 438(7066), 355-359. \doi{10.1038/nature04153}
+#'
 #' Kucharski, A. J., Russell, T. W., Diamond, C., Liu, Y., Edmunds, J.,
 #' Funk, S. & Eggo, R. M. (2020). Early dynamics of transmission and control
 #' of COVID-19: a mathematical modelling study. The Lancet Infectious Diseases,
@@ -27,7 +35,13 @@
 #'
 #' @examples
 #' probability_epidemic(R = 1.5, k = 0.1, num_init_infect = 10)
-probability_epidemic <- function(R, k, num_init_infect, ..., offspring_dist) {
+probability_epidemic <- function(R,
+                                 k,
+                                 num_init_infect,
+                                 ind_control = 0,
+                                 pop_control = 0,
+                                 ...,
+                                 offspring_dist) {
   input_params <- missing(R) && missing(k)
   if (!xor(input_params, missing(offspring_dist))) {
     stop("One of R and k or <epidist> must be supplied.", call. = FALSE)
@@ -44,6 +58,8 @@ probability_epidemic <- function(R, k, num_init_infect, ..., offspring_dist) {
   checkmate::assert_number(R, lower = 0, finite = TRUE)
   checkmate::assert_number(k, lower = 0)
   checkmate::assert_count(num_init_infect)
+  checkmate::assert_number(ind_control, lower = 0, upper = 1)
+  checkmate::assert_number(pop_control, lower = 0, upper = 1)
 
   # change Inf k to 1e10 to prevent issue with grid search
   if (is.infinite(k)) k <- 1e10
@@ -57,7 +73,10 @@ probability_epidemic <- function(R, k, num_init_infect, ..., offspring_dist) {
     # set up grid search
     ss <- seq(0.001, 0.999, 0.001)
     # define loss function
-    calculate_prob <- abs((1 + (R / k) * (1 - ss))^(-k) - ss)
+    calculate_prob <- abs(
+      ind_control + (1 - ind_control) *
+        (1 + (((1 - pop_control) * R) / k) * (1 - ss))^(-k) - ss
+    )
     # calculate probability of extinction
     prob_est <- ss[which.min(calculate_prob)]
   }
@@ -89,12 +108,20 @@ probability_epidemic <- function(R, k, num_init_infect, ..., offspring_dist) {
 #'
 #' @examples
 #' probability_extinct(R = 1.5, k = 0.1, num_init_infect = 10)
-probability_extinct <- function(R, k, num_init_infect, ..., offspring_dist) {
+probability_extinct <- function(R,
+                                k,
+                                num_init_infect,
+                                ind_control,
+                                pop_control,
+                                ...,
+                                offspring_dist) {
   # input checking done in probability_epidemic
   1 - probability_epidemic(
     R = R,
     k = k,
     num_init_infect = num_init_infect,
+    ind_control = ind_control,
+    pop_control = pop_control,
     ...,
     offspring_dist = offspring_dist
   )
