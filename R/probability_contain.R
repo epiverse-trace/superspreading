41614ff0b6e9bf097dfe1b0d03644616505dd2e7
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-12-14 10:51:20 +0000

    added ind_control and pop_control to probability_contain and num_init_infect > 1 for stochastic, relates #13

--- R/probability_contain.R
+++ R/probability_contain.R
@@ -4,10 +4,6 @@
 #' not reaching the `case_threshold` (default = 100).
 #'
 #' @inheritParams probability_epidemic
-#' @param control Control strength, 0 is no control measures, 1 is complete
-#' control.
-#' @param control_type Either `"population"` or `"individual"` for
-#' population-level or individual-level control measures.
 #' @param stochastic Whether to use a stochastic branching process model or the
 #' probability of extinction.
 #' @param ... arguments to be passed to [bpmodels::chain_sim()].
@@ -25,8 +21,11 @@
 #'
 #' @examples
 #' probability_contain(R = 1.5, k = 0.5, control = 1)
-probability_contain <- function(R, k, num_init_infect = 1, control,
-                                control_type = c("population", "individual"),
+probability_contain <- function(R,
+                                k,
+                                num_init_infect = 1,
+                                ind_control = 0,
+                                pop_control = 0,
                                 stochastic = TRUE,
                                 ...,
                                 case_threshold = 100,
@@ -44,20 +43,14 @@ probability_contain <- function(R, k, num_init_infect = 1, control,
   checkmate::assert_number(R, lower = 0, finite = TRUE)
   checkmate::assert_number(k, lower = 0)
   checkmate::assert_count(num_init_infect)
-  checkmate::assert_number(control, lower = 0, upper = 1)
+  checkmate::assert_number(ind_control, lower = 0, upper = 1)
+  checkmate::assert_number(pop_control, lower = 0, upper = 1)
   checkmate::assert_logical(stochastic, any.missing = FALSE, len = 1)
   checkmate::assert_number(case_threshold, lower = 1)
 
-  control_type <- match.arg(control_type)
-  if (control_type == "population") {
-    R <- (1 - control) * R
-  } else {
-    stop("individual-level controls not yet implemented", call. = FALSE)
-  }
-
-  if (num_init_infect != 1) {
+  if (ind_control > 0 && stochastic) {
     stop(
-      "Multiple introductions is not yet implemented for probability_contain",
+      "individual-level control not yet implemented for stochastic calculation",
       call. = FALSE
     )
   }
@@ -68,22 +61,27 @@ probability_contain <- function(R, k, num_init_infect = 1, control,
       n = 1e5,
       offspring = "nbinom",
       size = k,
-      mu = R,
+      mu = (1 - pop_control) * R,
       infinite = case_threshold
     )
 
     # replace default args if in dots (remove args not for chain_sim)
     args <- utils::modifyList(args, list(...)[...names() %in% names(args)])
 
-    chain_size <- do.call(
-      bpmodels::chain_sim,
-      args
+    chain_size <- lapply(
+      seq_len(num_init_infect),
+      function(x) do.call(bpmodels::chain_sim, args)
     )
-
-    prob_contain <- sum(!is.infinite(chain_size)) / length(chain_size)
+    control_chain_size <- lapply(chain_size, is.infinite)
+    control_chain_size <- Reduce(f = `+`, x = control_chain_size)
+    prob_contain <- sum(control_chain_size == 0) / length(control_chain_size)
   } else {
     prob_contain <- probability_extinct(
-      R = R, k = k, num_init_infect = num_init_infect
+      R = R,
+      k = k,
+      num_init_infect = num_init_infect,
+      ind_control = ind_control,
+      pop_control = pop_control
     )
   }
   return(prob_contain)
