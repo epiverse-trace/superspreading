4668c9a1b32efbf1b464869ab5c5cdfc357d68db
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-12-14 11:21:12 +0000

    updated probability_contain tests

--- tests/testthat/test-probability_contain.R
+++ tests/testthat/test-probability_contain.R
@@ -1,21 +1,67 @@
-test_that("probability_contain works as expected", {
+test_that("probability_contain works as expected for deterministic", {
   prob_contain <- probability_contain(
-    R = 1.5, k = 0.5, num_init_infect = 1, control = 0
+    R = 1.5, k = 0.5, num_init_infect = 1
+  )
+  expect_equal(prob_contain, 0.768)
+})
+
+test_that("probability_contain works as expected for stochastic", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 1, stochastic = TRUE
   )
   # larger tolerance for stochastic variance
   expect_equal(prob_contain, 0.7672, tolerance = 1e-2)
 })
 
-test_that("probability_contain works as expected for deterministic", {
+test_that("probability_contain works as expected for population control", {
   prob_contain <- probability_contain(
-    R = 1.5, k = 0.5, num_init_infect = 1, control = 0, stochastic = FALSE
+    R = 1.5, k = 0.5, num_init_infect = 1, pop_control = 0.1
   )
-  expect_equal(prob_contain, 0.768)
+  expect_equal(prob_contain, 0.821)
+})
+
+test_that("probability_contain works as expected for individual control", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 1, ind_control = 0.1
+  )
+  expect_equal(prob_contain, 0.839)
+})
+
+test_that("probability_contain works as expected for stochastic pop control", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 1, pop_control = 0.1, stochastic = TRUE
+  )
+  expect_equal(prob_contain, 0.819, tolerance = 1e-2)
+})
+
+test_that("probability_contain works as expected for both controls", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 1, ind_control = 0.1, pop_control = 0.1
+  )
+  expect_equal(prob_contain, 0.892)
+})
+
+test_that("probability_contain works as expected for ind control multi init", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 5, ind_control = 0.1
+  )
+  expect_equal(prob_contain, 0.4157285)
+})
+
+test_that("probability_contain works as expected for pop control multi init", {
+  prob_contain <- probability_contain(
+    R = 1.5, k = 0.5, num_init_infect = 5, pop_control = 0.1,
+  )
+  expect_equal(prob_contain, 0.373006, tolerance = 1e-2)
 })
 
 test_that("probability_contain works as expected for different threshold", {
   prob_contain <- probability_contain(
-    R = 1.5, k = 0.5, num_init_infect = 1, control = 0, case_threshold = 50
+    R = 1.5,
+    k = 0.5,
+    num_init_infect = 1,
+    stochastic = TRUE,
+    case_threshold = 50
   )
   # larger tolerance for stochastic variance
   expect_equal(prob_contain, 0.76255, tolerance = 1e-2)
@@ -23,7 +69,11 @@ test_that("probability_contain works as expected for different threshold", {
 
 test_that("probability_contain works as when using dots", {
   prob_contain <- probability_contain(
-    R = 1.5, k = 0.5, num_init_infect = 1, control = 0, n = 1e4, infinite = 50
+    R = 1.5,
+    k = 0.5,
+    num_init_infect = 1,
+    n = 1e4,
+    infinite = 50
   )
   # larger tolerance for stochastic variance
   expect_equal(prob_contain, 0.7606, tolerance = 0.1)
@@ -31,28 +81,10 @@ test_that("probability_contain works as when using dots", {
 
 test_that("probability_contain works as when using dots with incorrect name", {
   prob_contain <- probability_contain(
-    R = 1.5, k = 0.5, num_init_infect = 1, control = 0, random = 100
+    R = 1.5, k = 0.5, num_init_infect = 1, random = 100
   )
   # larger tolerance for stochastic variance
   expect_equal(prob_contain, 0.76669, tolerance = 1e-2)
-})
-
-test_that("probability_contain fails as expected", {
-  expect_error(
-    probability_contain(R = 1, k = 1, num_init_infect = 2, control = 1),
-    regexp = "(Multiple introductions is not yet implemented)"
-  )
-
-  expect_error(
-    probability_contain(
-      R = 1,
-      k = 1,
-      num_init_infect = 2,
-      control = 1,
-      control_type = "individual"
-    ),
-    regexp = "individual-level controls not yet implemented"
-  )
 })
 
 test_that("probability_contain works with <epidist>", {
@@ -67,17 +99,49 @@ test_that("probability_contain works with <epidist>", {
   expect_equal(
     probability_contain(
       num_init_infect = 1,
-      control = 0.1,
-      stochastic = FALSE,
+      pop_control = 0.1,
       offspring_dist = edist
     ),
     0.904
+  )
+
+  expect_equal(
+    probability_contain(
+      num_init_infect = 1,
+      ind_control = 0.1,
+      offspring_dist = edist
+    ),
+    0.913
+  )
+
+  expect_equal(
+    probability_contain(
+      num_init_infect = 5,
+      ind_control = 0.1,
+      pop_control = 0.1,
+      offspring_dist = edist
+    ),
+    0.71842137
+  )
+})
+
+test_that("probability_contain fails as expected", {
+  expect_error(
+    probability_contain(
+      R = 1,
+      k = 1,
+      num_init_infect = 2,
+      ind_control = 1,
+      stochastic = TRUE
+    ),
+    regexp =
+      "individual-level control not yet implemented for stochastic calculation"
   )
 })
 
 test_that("probability_contain fails without R and k or <epidist>", {
   expect_error(
-    probability_contain(num_init_infect = 1, control = 0.5),
+    probability_contain(num_init_infect = 1, pop_control = 0.5),
     regexp = "One of R and k or <epidist> must be supplied."
   )
 })
