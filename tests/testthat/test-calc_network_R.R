b63c800939679690e70386de73bcf860234625ff
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-12-14 15:29:14 +0000

    added tests for calc_network_R

---
+++ tests/testthat/test-calc_network_R.R
@@ -0,0 +1,22 @@
+test_that("calc_network_R works as expected", {
+  R <- calc_network_R(
+    mean_num_contact = 15,
+    sd_num_contact = 50,
+    infect_duration = 1,
+    prob_transmission = 1,
+    age_range = c(15, 75)
+  )
+  expect_equal(R, c(R = 0.25, R_var = 3.0277778))
+})
+
+test_that("calc_network_R gives equal values when sd is zero", {
+  R <- calc_network_R(
+    mean_num_contact = 15,
+    sd_num_contact = 0,
+    infect_duration = 1,
+    prob_transmission = 1,
+    age_range = c(15, 75)
+  )
+  expect_equal(R, c(R = 0.25, R_var = 0.25))
+})
+
