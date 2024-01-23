9a2bea913ce5fc8b1af698f2e088b7c7dd3f089c
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-01-23 17:17:21 +0000

    added tests for .fit

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -85,3 +85,36 @@ test_that("ic_tbl fails as expected", {
   pois_fit <- unclass(pois_fit)
   expect_error(ic_tbl(pois_fit), regexp = "Input objects must be <fitdist>")
 })
+
+test_that(".fit works as expected" ,{
+  func <- function(x) {
+    (x - 3)^2 + 5
+  }
+  expect_equal(.fit(func = func, fit_method = "optim", upper = 5), 3)
+})
+
+test_that(".fit fails as expected", {
+  expect_error(
+    .fit(func = "function", fit_method = "optim"),
+    regexp = "func must be a function"
+  )
+  func <- function(x) {
+    (x - 3)^2 + 5
+  }
+  expect_error(
+    .fit(func = func, fit_method = "other"),
+    regexp = "(arg)*(should be one of)*(optim)*(grid)"
+  )
+  expect_error(
+    .fit(func = func, fit_method = "optim", a = 1, a = 2),
+    regexp = "(Arguments)*(must have unique names)"
+  )
+  expect_error(
+    .fit(func = func, fit_method = "optim", mthod = "L-BFGS-B"),
+    regexp = "Arguments supplied in `...` not valid"
+  )
+  expect_error(
+    .fit(func = func, fit_method = "grid", method = "L-BFGS-B"),
+    regexp = "Arguments supplied in `...` not valid"
+  )
+})
