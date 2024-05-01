f28076cf63983feb91a281295728beb74e8ffae2
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-05-01 10:36:33 +0100

    update proportion_transmission simulate arg doc, closes #94

--- R/proportion_transmission.R
+++ R/proportion_transmission.R
@@ -21,7 +21,8 @@
 #' @param percent_transmission A `number` of the percentage transmission
 #' for which a proportion of cases has produced.
 #' @param simulate A `logical` whether the calculation should be done
-#' numerically (i.e. simulate secondary contacts) or analytically.
+#' numerically (i.e. simulate secondary contacts) or analytically. Default is
+#' `FALSE` which uses the analytical calculation.
 #'
 #' @return A `<data.frame>` with the value for the proportion of cases for a
 #' given value of R and k.
