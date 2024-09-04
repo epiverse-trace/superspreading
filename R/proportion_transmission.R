186def053b21c9ed7eec0ce20bfb1ba3adc60bfc
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-09-04 18:00:25 +0100

    sentence case with full stops for errors and messages in proportion_transmission

--- R/proportion_transmission.R
+++ R/proportion_transmission.R
@@ -148,12 +148,12 @@ proportion_transmission <- function(R, k,
   checkmate::assert_numeric(R, lower = 0, finite = TRUE)
   checkmate::assert_numeric(k, lower = 0)
   # checkmate lower is >= 0 but k must be > 0
-  stopifnot("k cannot be zero" = k != 0)
+  stopifnot("k cannot be zero." = k != 0)
   # k must be finite for analytical p_80 and t_20 methods
   if (any(is.infinite(k))) {
     k[is.infinite(k)] <- 1e5
     message(
-      "Infinite values of k are being approximated by 1e5 for calculations"
+      "Infinite values of k are being approximated by 1e5 for calculations."
     )
   }
   checkmate::assert_number(percent_transmission, lower = 0, upper = 1)
@@ -258,7 +258,7 @@ proportion_transmission <- function(R, k,
     k[k > 1e7] <- 1e7
     message(paste(
       "Values of k > 1e7 are set to 1e7 to due to numerical integration",
-      "issues at higher values"
+      "issues at higher values."
     ))
   }
   u <- solve_for_u(prop = percent_transmission, R = R, k = k)
