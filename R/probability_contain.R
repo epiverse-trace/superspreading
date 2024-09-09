402ed5b44d9d541fc34ca94a737e217ffe2a29b2
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-09-09 15:32:33 +0100

    update probability_contain to use .chain_sim and add outbreak_time and generation_time args, and rename stochastic to simulate

--- R/probability_contain.R
+++ R/probability_contain.R
@@ -1,22 +1,56 @@
 #' Probability that an outbreak will be contained
 #'
-#' @description Containment is defined as the size of the transmission chain
-#' not reaching the `case_threshold` (default = 100).
+#' @description Outbreak containment is defined as outbreak extinction when
+#' `simulate = FALSE`. When `simulate = FALSE`, [probability_contain()] is
+#' equivalent to calling [probability_extinct()].
+#'
+#' When `simulate = TRUE`, outbreak containment is defined by the
+#' `case_threshold` (default = 100) and `outbreak_time` arguments.
+#' Firstly, `case_threshold` sets the size of the transmission chain below
+#' which the outbreak is considered contained. Secondly, `outbreak_time` sets
+#' the time duration from the start of the outbreak within which the outbreak
+#' is contained if there is no more onwards transmission beyond this time.
+#' When setting an `outbreak_time`, a `generation_time` is also required.
+#' `case_threshold` and `outbreak_time` can be jointly set.
+#' Overall, when `simulate = TRUE`, containment is defined as the size and
+#' time duration of a transmission chain not reaching the `case_threshold`
+#' and `outbreak_time`, respectively.
 #'
 #' @details
-#' When using `stochastic = TRUE`, the default arguments to simulate the
-#' transmission chains with [bpmodels::chain_sim()] are `1e5` replicates,
+#' When using `simulate = TRUE`, the default arguments to simulate the
+#' transmission chains with [chain_sim()] are `1e5` replicates,
 #' a negative binomial (`nbinom`) offspring distribution, parameterised with
 #' `R` (and `pop_control` if > 0) and `k`.
 #'
+#' When setting the `outbreak_time` argument, the `generation_time` argument is
+#' also required. The `generation_time` argument requires a random number
+#' generator function. For example, if we assume the generation time is
+#' lognormally distributed with `meanlog = 1` and `sdlog = 1.5`, then we can
+#' define the `function` to pass to `generation_time` as:
+#' ```r
+#' function(x) rlnorm(x, meanlog = 1, sdlog = 1.5)
+#' ```
+#'
+#'
 #' @inheritParams probability_epidemic
-#' @param stochastic Whether to use a stochastic branching process model or the
-#' analytical probability of extinction. Default (`FALSE`) is to use the
-#' analytical calculation.
+#' @param simulate A `logical` boolean determining whether the probability
+#' of containment is calculated analytically or numerically using a stochastic
+#' branching process model. Default is `FALSE` which calls
+#' [probability_extinct()], setting to `TRUE` uses a branching process and
+#' enables setting the `case_threshold`, `outbreak_time` and `generation_time`
+#' arguments.
 #' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Named elements to replace
-#' default arguments in [bpmodels::chain_sim()]. See details.
+#' default arguments in [chain_sim()]. See details.
 #' @param case_threshold A number for the threshold of the number of cases below
-#' which the epidemic is considered contained.
+#' which the epidemic is considered contained. `case_threshold` is only used
+#' when `simulate = TRUE`.
+#' @param outbreak_time A number for the time since the start of
+#' the outbreak to determine if outbreaks are contained within a given period
+#' of time. `outbreak_time` is only used when `simulate = TRUE`.
+#' @param generation_time A `function` to generate generation times. The
+#' function must have a single argument and return a `numeric` vector with
+#' generation times. See details for example. The `function` can be defined or
+#' anonymous. `generation_time` is only used when `simulate = TRUE`.
 #'
 #' @return A `number` for the probability of containment.
 #' @export
@@ -46,22 +80,36 @@
 #'
 #' # multi initial infections with population-level control measures
 #' probability_contain(R = 1.5, k = 0.5, num_init_infect = 5, pop_control = 0.1)
+#'
+#' # probability of containment within a certain amount of time
+#' # this requires parameterising a generation time
+#' gt <- function(n) {
+#'   rlnorm(n, meanlog = 1, sdlog = 1.5)
+#' }
+#' probability_contain(
+#'   R = 1.5,
+#'   k = 0.5,
+#'   num_init_infect = 1,
+#'   stochastic = TRUE,
+#'   case_threshold = 100,
+#'   outbreak_time = 20,
+#'   generation_time = gt
+#' )
 probability_contain <- function(R,
                                 k,
                                 num_init_infect,
                                 ind_control = 0,
                                 pop_control = 0,
-                                stochastic = FALSE,
+                                simulate = FALSE,
                                 ...,
                                 case_threshold = 100,
+                                outbreak_time = Inf,
+                                generation_time = NULL,
                                 offspring_dist) {
   # capture dynamic dots
   dots <- rlang::dots_list(..., .ignore_empty = "none", .homonyms = "error")
   dots_names <- names(dots)
-
-  # bpmodels::chain_sim cannot be called within formals() as it's not in base
-  bp_func <- bpmodels::chain_sim
-  bp_args <- names(formals(bp_func))
+  bp_args <- names(formals(.chain_sim))
   # check arguments in dots match arg list
   stopifnot(
     "Arguments supplied in `...` not valid" =
@@ -86,36 +134,62 @@ probability_contain <- function(R,
   checkmate::assert_count(num_init_infect)
   checkmate::assert_number(ind_control, lower = 0, upper = 1)
   checkmate::assert_number(pop_control, lower = 0, upper = 1)
-  checkmate::assert_logical(stochastic, any.missing = FALSE, len = 1)
+  checkmate::assert_logical(simulate, any.missing = FALSE, len = 1)
   checkmate::assert_number(case_threshold, lower = 1)
+  checkmate::assert_number(outbreak_time, lower = 0)
 
   if (ind_control > 0 && stochastic) {
     stop(
-      "individual-level control not yet implemented for stochastic calculation",
+      "individual-level control not yet implemented for `simulate` calculation",
+      call. = FALSE
+    )
+  }
+  if (is.finite(outbreak_time) && is.null(generation_time)) {
+    stop(
+      "`generation_time` is required to calculate the probability of ",
+      "containment within an `outbreak_time`",
       call. = FALSE
     )
   }
 
   if (stochastic) {
-    # arguments for chain_sim()
+    nsim <- 1e5
+    # arguments for .chain_sim()
     args <- list(
-      n = 1e5,
+      n = nsim,
       offspring = "nbinom",
       size = k,
       mu = (1 - pop_control) * R,
-      infinite = case_threshold
+      stat_threshold = case_threshold
     )
+    if (!is.null(generation_time)) {
+      args$generation_time <- generation_time
+    }
 
     # replace default args if in dots
     args <- utils::modifyList(args, dots)
 
-    chain_size <- lapply(
+    # lapply() wraps output <data.frame> in a list so unpack with `[[`
+    chain <- lapply(
       seq_len(num_init_infect),
-      function(x) do.call(bpmodels::chain_sim, args)
-    )
-    control_chain_size <- lapply(chain_size, is.infinite)
-    control_chain_size <- Reduce(f = `+`, x = control_chain_size)
-    prob_contain <- sum(control_chain_size == 0) / length(control_chain_size)
+      function(x) do.call(chain_sim, args)
+    )[[1]]
+
+    # size is the total offspring produced by a chain
+    chain_size <- as.vector(table(chain$n), mode = "numeric")
+
+    if (is.finite(outbreak_time)) {
+      # subset chains that were under case_threshold by outbreak_time
+      contain_within_t <- aggregate(
+        time ~ n,
+        data = chain,
+        FUN = function(x) max(x) < outbreak_time
+      )$time
+      control_chain_size <- sum(contain_within_t & chain_size < case_threshold)
+    } else {
+      control_chain_size <- sum(chain_size < case_threshold)
+    }
+    prob_contain <- control_chain_size / nsim
   } else {
     prob_contain <- probability_extinct(
       R = R,
