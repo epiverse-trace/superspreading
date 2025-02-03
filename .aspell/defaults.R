db731d35a84dd655350761168bdf47c7c9cc5262
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-02-03 15:34:36 +0000

    add .aspell/ for CRAN spell checking

---
+++ .aspell/defaults.R
@@ -0,0 +1,12 @@
+# see `?aspell` and `?aspell-utils` for help
+# run below function to update aspell dictionary
+# saveRDS(
+#   c("Kremer", "et", "al", "Kucharski", "Lloyd-Smith"),
+#   file = ".aspell/superspreading.rds",
+#   version = 2
+# )
+description <- list(
+  encoding = "UTF-8",
+  language = "en",
+  dictionaries = c("en_stats", "superspreading")
+)
