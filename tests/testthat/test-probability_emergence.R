a3cfe33e8c4ce30ad366c600d371961631d1b31d
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-07-15 10:27:18 +0100

    add test for num_init_infect > 1 for probability_emergence

--- tests/testthat/test-probability_emergence.R
+++ tests/testthat/test-probability_emergence.R
@@ -1,13 +1,17 @@
 test_that("probability_emergence works for R <= 1 & mutation_rate = 0", {
   expect_identical(
-    probability_emergence(R_wild = 0.5, R_mutant = 1.5, mutation_rate = 0),
+    probability_emergence(
+      R_wild = 0.5, R_mutant = 1.5, mutation_rate = 0, num_init_infect = 1
+    ),
     0
   )
 })
 
 test_that("probability_emergence works for R_mutant > 1", {
   expect_snapshot(
-    probability_emergence(R_wild = 0.5, R_mutant = 1.5, mutation_rate = 0.1)
+    probability_emergence(
+      R_wild = 0.5, R_mutant = 1.5, mutation_rate = 0.1, num_init_infect = 1
+    )
   )
 })
 
@@ -16,14 +20,17 @@ test_that("probability_emergence works for multi-type ", {
     probability_emergence(
       R_wild = 0.5,
       R_mutant = c(0.5, 0.5, 1.5),
-      mutation_rate = 0.1
+      mutation_rate = 0.1,
+      num_init_infect = 1
     )
   )
 })
 
 test_that("probability_emergence is approx 1 for R >> 1", {
   expect_equal(
-    probability_emergence(R_wild = 10, R_mutant = 10, mutation_rate = 0.1),
+    probability_emergence(
+      R_wild = 10, R_mutant = 10, mutation_rate = 0.1, num_init_infect = 1
+    ),
     1,
     tolerance = 0.01
   )
@@ -33,14 +40,21 @@ test_that("emergence is equal to epidemic for R > 1 & no mutation", {
   emerge <- c()
   epidemic <- c()
   for (R_i in c(2, 5, 10)) {
-    emerge <- c(
-      emerge,
-      probability_emergence(R_wild = R_i, R_mutant = R_i, mutation_rate = 0)
-    )
-    epidemic <- c(
-      epidemic,
-      probability_epidemic(R = R_i, k = Inf, num_init_infect = 1)
-    )
+    for (num_init_infect in c(1, 5, 10)) {
+      emerge <- c(
+        emerge,
+        probability_emergence(
+          R_wild = R_i,
+          R_mutant = R_i,
+          mutation_rate = 0,
+          num_init_infect = num_init_infect
+        )
+      )
+      epidemic <- c(
+        epidemic,
+        probability_epidemic(R = R_i, k = Inf, num_init_infect = num_init_infect)
+      )
+    }
   }
   expect_equal(emerge, epidemic, tolerance = 0.01)
 })
